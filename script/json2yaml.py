2623acd4f5d4ad762f6e73e4053bba269a417ec9
lunzhipenxil <lunzhipenxil@gmail.com>
2020-01-17 22:33:09 +0800

    RightKey update

--- script/json2yaml.py
+++ script/json2yaml.py
@@ -22,9 +22,10 @@ from tkinter import filedialog
 from tkinter import messagebox
 from tkinter import ttk
 import webbrowser
+import pyperclip
 
 favicon_ico = "AAABAAEAICAAAAEAIACoEAAAFgAAACgAAAAgAAAAQAAAAAEAIAAAAAAAABAAAMMOAADDDgAAAAAAAAAAAAA3LBr/Py8e/0M0I/9CNSP/QzYl/0xEM/9gWEj/cGVW/3xxYP+Lfmz/kYRz/5SHdv+TiXb/k4p2/5KKd/+Rinf/k4h1/4+Ecf+LgW3/iX5r/4Z7af+CdmX/fnJf/3dpWP9uYVH/ZltJ/1dOO/9KQS//QDUj/z0wHv88Lx7/OS0a/zguHf9DMyH/QjYk/1hSQ/+CfnL/lJKG/5qVhf+XiXf/j39t/39xXP90ZlD/cWVP/3BgTP9vX0z/c2ZU/4B1Y/9+c2P/c2ZT/21fTP9oWkj/Z1lF/2ZXRf9oWkX/ZllE/2daRv9yZFH/f3Nf/4J5Zf98cmP/VEs//zowIP87Lhz/PjIf/0I1Iv90bmT/mpqR/6Cfkf+IgGz/b2FD/2VXPf9cTDf/YVI//2ZXQf9oVkD/b19R/6Gbk/+ZlIz/lJKK/5mWjv+CeGf/emtZ/25eTv9oWUb/ZFRD/2JUP/9bTTr/Vkc0/1VEMv9VRDD/YFA6/4J4X/+Jh3f/gXt1/0U9Lv9ENiL/Rjkk/2RbTv+KhXb/mZSA/5mSev+Ge13/dGlM/2xfR/9tXkj/altB/3FlU/+vqqT/vb66/6+vqf+ytbP/tra2/6Oflf+FeWb/dWVR/2VYQf9kVkD/YlQ8/19RPf9dTzr/XlM7/2peRv9+dFv/lY57/42LgP93cGX/Rzsp/0Q3JP9JOyH/Rzcf/1NEMf9nXEr/em9c/5OHcv+nnYf/r6GM/6aXff+aj3j/uLmv/8vJy/+urKT/w8a+/8nNyf/Iycr/zMrJ/6OZif+Sgmr/j4Jo/5GDbP+Wi3H/npJ8/6Wbh/+noIv/nZWD/4h/bv9uY1L/UUUy/0EyHv9BMh3/RTgl/0k8JP9PPyj/VEMx/1hJNv9cTTj/YlNA/25iUf95aln/jYNx/8vLw//T0dD/qaCW/9rVz//j5eX/3drZ/93Z2//g4uT/3tzV/6aahv+ekXj/m452/5WKcf+MgWj/g3hh/3dqV/9iVET/UkQz/009J/9KOyT/SDom/0Q2JP9KPCj/TkAr/1BCLP9XSDT/Xk88/2VXQP9pW0X/aVxI/2teR/+9t7D/09LR/5iNff+KgGf/ysS4/9XUzf/v7/D/7+/w/9jW0v/JxLn/joNu/3JkRP9yZEX/bV4//2lZOf9iUzb/YlU8/2BROv9aSjX/VUYx/05AKf9IPCX/RTck/0w+Kf9ENSP/TT4p/1xONv9gUj3/ZlhC/2tdRv9xYk7/cmNN/8zJwv/LzMT/eGhN/4ZzWv9yZEf/qaOR////////////sqya/3JhRP+Cb1b/g3BQ/39rS/97Z0j/dmJC/3BfQ/9qXUX/YFM5/1lNMv9XSjH/T0Ir/0w8Jv9IOSP/SD0m/4h+cP95cGD/TkMt/2RVPv9nWkP/bV9I/3VmTv9rXUL/tLCl/+rt6v+fk33/gW5S/4NuUv+0q5z///////////+4sqL/fmtN/4dzVf99Z03/fGpP/3pmSP93ZEP/cV9F/2lbRP9hVDn/WU0x/1ZJLv9PQir/TD0m/0g5Iv9NQiz/2dXO//b49f+CfXD/WUkx/21eQ/9uY0z/dGdO/3BjRv+ln47/9Pf3/8jCs/+FdFf/h3NU/6edi//x7/D/6uzq/6+omf+Kdlb/h3da/5aRh/+0r6z/h3pf/3ZkRv91Y0n/bmFI/2hbQf9fUjn/WUsz/1JFK/9NQCf/STsk/0o8Jv9YSjv/19LN//f39P+Jf3H/YVU9/3RoTf93bFH/d2lM/5OLd//w9PX/4+Te/49+Zv+Sgmj/zsq+//Ht7//x8vT/raWT/4l2XP+soZf/xsjI/6+qo/+Aclj/f2pP/3hmTP9xZEr/aVxF/2VXQP9dTzj/UkQs/00/Jv9JOyT/UkQt/049Jf9qXkz/7Ovn//n59v+Mg3H/dWNH/3psUP91Z0//xsCz//P39v/09vT/rKCP/9PLwf//////4+Hb//Du6//d2M7/p52S/9DOzP+0rqf/g3Vd/35sTv+DblP/e2lN/3NnTf9tX0j/aFlD/11POP9TRS//T0Eq/0s9Jv9OQCj/Vkox/05AJ/+Dd2b//////8vFu/9uWTv/fXBX/8fBuf//////9vb2//X29v/y8+///v7//+Hf2P+VhXD/19DG//37/v/W1tH/tLGj/4d3XP+Ec1X/iHdY/4NxVf98ak//eGhK/3JiR/9qWkL/XlE4/1RHL/9RQyz/Sz0n/09CKP9WSS//X1Ez/2VUN//q6OT/5eHd/3ptWP/h3dL///////n5+//9+/z/+Pr6//3////QzMH/lYZs/5h+Yf+9s6b//////8nBuP+Pe2H/kn5e/457Xf+NemH/iHVc/31pTf95Z0j/cmFE/2pZPf9iVDf/WEsx/1NFL/9OQCr/UUUq/1lNM/9jUjb/ZVI3/9TSzP/6+vj/4t7c///////6/Pz//f/9//z9/P//////29XN/5WBZf+ciGr/nIdq/6mdif/s7Oz/urWp/5mCZ/+chmv/lH9i/49+Yf+FblP/fWZG/3xuVP9zY0f/bVo8/2VWOf9ZTTL/VUcx/1BCK/9USC3/WUwx/2RRNP9jTjD/nJN+//7++f/9/f7//f39//7//v/8/f3//f36/9jUyv+ej3f/oo5v/6ONcf+fi2//ppmC/+Ph3f/X2dL/opB3/5yGaf+TgWP/inhc/5yLeP+Jd1//dmlN/3FiRv9uXD7/aFc5/1xQNP9TRyz/T0Ep/1VKLf9ZSy7/XE0x/4Z4YP+IfWf/r6qd//z8+//8/P3//f37///////n5+T/ppuI/5yLcP+hi27/nIhu/52Lbf+hkHf/3tzS/+7w7v+ypJH/nohp/4p2Wf+3q5r/+fTy/9HIv/91ZEb/eWZH/3BdQP9rWTz/XlAz/1RILP9PQij/V0wu/0tAJ/+0rqP////////////r6ef/2dXT//7+/v/8/v3/0s7G/+7r5//w8Ov/nI56/8S8rP/s6uD/ppZ8/5eDaP/Y1Mv/9/r6/8vAs/+Kd2D/vK2f//f49P/y8/D/6efk/4V2W/93YkL/dV9D/2tXO/9eTzL/V0ou/1BEKP9OQiT/fHJh///+/f/8/v7/+vz7///////e29f/zMjA///////NyMD/urSl//P08f/p5uD//P79/+/q5v+Zh3D/wbem//T08v/0+Pb/5d7W/8W8sf/4/Pj/6+ro/9XRyP/7/v7/n5SD/29cPv9zYkX/bFk8/2JSNf9ZTC//UkYq/0o+IP+8t6r///////v6+////vz/+v7+//X29v+Kfmz/vLSm///////Kxb7/1NDJ///////X08n/jXth/8W6p//8/f//9vf5//b39v/2+PX//f////T18P+onoz/o5iE/////v/Iwbv/c2BE/3RjRf9sWj3/YlM2/1hNMP9RRSn/Sz0j/4mDcv//////+/v8//n6+P/+////6+nl/4t7Yv+Aclb/y8e8///////8/Pz/+vz+/6+gkP/Duqr///////r8/P/8/Pz/+fr6//z////x8u7/qJ6N/49+YP+HeV3/3NrR/+zp5f92aE7/dGFD/2tYPP9fUDT/Vkou/1FFKf9SQiv/UEUr/8nHu////////f7+/////f+mnYr/fW5S/4h7X/+MfmX/2NbL/+Dh3v/39/X/6+3p//r7+v/9/P7//v7+//z8/P/+////9PLs/6qfif+QfV//jXxe/4JzWP+YjXj/m5B//3VkSv9wXkH/ZVQ3/1pNMf9TRyv/T0Mo/1NGKv9WSSv/XFI5/5GJdv+noJD/hXtn/25fQv9+b1b/g3Na/418Yv+JemX/i4Bt/9XSyf//////+fz7//7+/v/8/P3//////+/u6f+mmIL/kYJd/456Vv+HdFL/gnBT/3pnSv90ZUn/cmRI/2hbQP9gVDj/WU0x/1RGLv9PQir/TkQl/1NHLP9XTDD/V0or/15MLP9nVjj/cGNJ/3hrUv+AcFb/hXdh/6adkP/Jwrb/w760/+Ph3f//////+/v6///////n5d//npN5/5F7Yf+GeWH/jIBj/4h4WP96ak3/dGVI/3FkSv9rXET/ZFY+/1tPNf9WSTD/UEIr/0w/KP9LPiX/UEMo/1VJLf9dUTT/ZVc7/2ZYQP9oW0b/eGlR/3lqUv+fmov/+fr4////////////4uDf/+bl5P//////7+3r/5OKef+LfWD/iXdc/5qRgv/u7ub/ubKh/29hRP9wY0b/a11F/2hYQv9gUDv/V0kz/1NFLv9OQCr/Sz0n/0U3JP9LPSX/UEIp/1dILv9YSzL/XlE7/2VYQv9zY03/e21Y/+Lg2///////+/v7//39/v//////xcG+/9/c2P/+////ubKp/4R0Yf+on4z/+Pn0//v6+/+ck4b/bV1E/2haQv9kVj//YFI7/1tMNv9URS//T0Ar/0k6J/9FNyX/RDUk/0g5J/9MPSr/U0Qw/1RFMf9ZSjf/YFNA/2lbRP9/cl//8fDt//7+///9/f3//fz8///////V0cz/iH1s/+Ph3P//////0MzI//f29P/v7+z/lYuA/21dR/9pWkb/Xk88/2BRPf9cTTr/V0k1/1JEMP9NPiv/Rjgl/0I0Iv9AMiH/RTYm/0g5KP9PPy3/UEAv/1VENP9bTDv/ZlZC/2tdS//X1tD///////v7/P/9/f7//////62mmv9zZU//iYJ0/+3t6v//////8/Hv/4yDc/9oWUX/bFpH/2FRQ/9bSjr/WEg3/1VFNP9QQS//Sjwr/0g6J/9ENiP/PC8d/zouHP8/MSD/RDYj/0o9Kf9MPCr/T0Av/1dHN/9dTjv/YVNA/4eAdP/m5eH//P38//n7+f+3taz/aF5M/2teS/9pXEr/g3pr/8fDuv+LgnX/aVlG/2VURP9fTz7/Wkw6/1RFNP9URTP/UEEw/0w9LP9FNyX/QTMg/z8yH/82Khj/MSkY/zYuHP88MB//QTcl/0Y4Jv9HOyn/UEMy/1VHNP9aSzj/XlJB/2liUv+De27/e3Fk/1xRP/9cTjz/Wkw6/11POv9iVED/XE8+/15RQf9fUT//WUo5/1dKOP9TRjT/T0Ev/09BL/9KPSr/RDgm/0E1I/85LRv/NSoY/zEnFf8rKBb/LysZ/zUtHP85MiD/PjMh/z83JP9HPSv/UUMx/1VGM/9XSzj/WE89/1RIN/9YSDj/Wks5/1VHNf9URjT/UkUx/1NFMv9YSzr/Vko5/1NINf9PQzH/T0Mx/1BEMv9NPy3/SDso/0I4JP89NSL/OTEf/zQtGv8uJxX/LCUU/ykjEf8rJRT/MScZ/zErHP82LR7/Misa/y4nFf9EOyn/UEMx/1JDM/9PRTb/TUEw/1FDMf9RQjD/TD4q/0k7Kv9JPCr/Rzoo/0k8K/9JQDD/Rz4t/0Y8Kv9GOir/STwr/0U7J/8/NSP/NzAf/zQuHf8xLBr/LSgW/yokE/8mHxH/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="
-j2y_version = "1.0.3.20200117.1"
+j2y_version = "1.0.4.20200117.2"
 project_site = "http://benzenpenxil.xyz/json2yaml-for-dice/"
 
 #class type_system_info:
@@ -117,6 +118,7 @@ def json2yaml_work():
     root.update()
     output_str = "#必要信息\nname: " + deck.name
     output_str += "\nauthor: " + deck.author
+    output_str += "(使用Json2Yaml转换生成)"
     output_str += "\nversion: " + deck.version
     output_str += "\ncommand: " + deck.command
     output_str += "\ndesc: " + deck.desc
@@ -287,17 +289,23 @@ def tran_save():
         file_path = tkinter.filedialog.askdirectory(title="选择保存路径")
         if len(file_path) != 0:
             #print(file_path)
-            deck.name = t1.get()
-            deck.author = t2.get()
-            deck.version = t3.get()
-            deck.command = t1.get()
-            deck.desc = t4.get()
-            deck.includes = t5.get().split(",")
-            deck.default = t6.get()
-            output_file_name = file_path + "/" + deck.name
-            #print(deck.includes)
-            json2yaml_work()
-            tkinter.messagebox.showinfo("完成","已保存到 " + output_file_name)
+            try:
+                deck.name = t1.get()
+                deck.author = t2.get()
+                deck.version = t3.get()
+                deck.command = t1.get()
+                deck.desc = t4.get()
+                deck.includes = t5.get().split(",")
+                deck.default = t6.get()
+            except UnicodeDecodeError as error_info:
+                tkinter.messagebox.showerror("UnicodeDecodeError", error_info)
+                progress_obj["value"] = 0
+                root.update()
+            else:
+                output_file_name = file_path + "/" + deck.name
+                #print(deck.includes)
+                json2yaml_work()
+                tkinter.messagebox.showinfo("完成","已保存到 " + output_file_name)
         else:
             tkinter.messagebox.showwarning("警告", "请选择保存路径！")
             progress_obj["value"] = 0
@@ -306,6 +314,20 @@ def tran_save():
         tkinter.messagebox.showwarning("警告", "请先选择要转换的Json文件！")
         progress_obj["value"] = 0
         root.update()
+
+def clear_conf():
+    global t1
+    global t2
+    global t3
+    global t4
+    global t5
+    global t6
+    t1.set("")
+    t2.set("")
+    t3.set("")
+    t4.set("")
+    t5.set("")
+    t6.set("")
 
 def show_info():
     tkinter.messagebox.showinfo("Json2Yaml By BenzenPenxil","Json2Yaml基于Python\n\n项目主页：\n" + project_site + "\n\n作者：仑质(BenzenPenxil)\n版本：" + j2y_version + "\n有问题请联系QQ：137334701")
@@ -316,6 +338,106 @@ def show_project_site():
         webbrowser.open(project_site)
     except webbrowser.Error as error_info:
         tkinter.messagebox.showerror("webbrowser.Error", error_info)
+
+def tree_copy(obj, event=None):
+    length_select = len(obj.selection())
+    if length_select != 0:
+        str_select = obj.selection()[0]
+        str_select_len = len(str_select)
+        for i in range(1, str_select_len):
+            if str_select[-i] == "#":
+                pyperclip.copy(str_select[-str_select_len: - i])
+                break
+
+def tree_set_name(obj, event=None):
+    global t1
+    length_select = len(obj.selection())
+    if length_select != 0:
+        str_select = obj.selection()[0]
+        str_select_len = len(str_select)
+        for i in range(1, str_select_len):
+            if str_select[-i] == "#":
+                t1.set(str_select[-str_select_len: - i])
+                break
+
+def tree_add_includes(obj, event=None):
+    global t5
+    length_select = len(obj.selection())
+    if length_select != 0:
+        str_select = obj.selection()[0]
+        str_select_len = len(str_select)
+        for i in range(1, str_select_len):
+            if str_select[-i] == "#":
+                if t5.get().split(",") != [""]:
+                    if str_select[-str_select_len: - i] in t5.get().split(","):
+                        pass
+                    else:
+                        tmp_t5_str = t5.get().split(",")
+                        tmp_t5_str.append(str_select[-str_select_len: - i])
+                        t5.set(",".join(tmp_t5_str))
+                else:
+                    t5.set(str_select[-str_select_len: - i])
+                break
+
+def tree_set_default(obj, event=None):
+    global t5
+    global t6
+    length_select = len(obj.selection())
+    if length_select != 0:
+        str_select = obj.selection()[0]
+        str_select_len = len(str_select)
+        for i in range(1, str_select_len):
+            if str_select[-i] == "#":
+                t6.set(str_select[-str_select_len: - i])
+                if t5.get().split(",") != [""]:
+                    if "default" in t5.get().split(","):
+                        pass
+                    else:
+                        tmp_t5_str = t5.get().split(",")
+                        tmp_t5_str.append("default")
+                        t5.set(",".join(tmp_t5_str))
+                else:
+                    t5.set("default")
+                break
+
+def tree_rightKey(event, obj):
+    tree_rightkey_menu.delete(0, tkinter.END)
+    tree_rightkey_menu.add_command(label="复制", command=lambda: tree_copy(obj, event))
+    tree_rightkey_menu.add_command(label="设为名称/指令", command=lambda: tree_set_name(obj, event))
+    tree_rightkey_menu.add_command(label="加入子指令", command=lambda: tree_add_includes(obj, event))
+    tree_rightkey_menu.add_command(label="设为default", command=lambda: tree_set_default(obj, event))
+    tree_rightkey_menu.post(event.x_root, event.y_root)
+
+def entry_cut(editor, event=None):
+    editor.event_generate("<<Cut>>")
+
+def entry_copy(editor, event=None):
+    editor.event_generate("<<Copy>>")
+
+def entry_paste(editor, event=None):
+    editor.event_generate('<<Paste>>')
+
+def entry_clear(editor, event=None):
+    if editor["textvariable"] == "PY_VAR1":
+        t1.set("")
+    elif editor["textvariable"] == "PY_VAR2":
+        t2.set("")
+    elif editor["textvariable"] == "PY_VAR3":
+        t3.set("")
+    elif editor["textvariable"] == "PY_VAR4":
+        t4.set("")
+    elif editor["textvariable"] == "PY_VAR5":
+        t5.set("")
+    elif editor["textvariable"] == "PY_VAR6":
+        t6.set("")
+
+def entry_rightKey(event, editor):
+    entry_rightkey_menu.delete(0, tkinter.END)
+    entry_rightkey_menu.add_command(label='剪切', command=lambda: entry_cut(editor))
+    entry_rightkey_menu.add_command(label='复制', command=lambda: entry_copy(editor))
+    entry_rightkey_menu.add_command(label='粘贴', command=lambda: entry_paste(editor))
+    entry_rightkey_menu.add_command(label='清空', command=lambda: entry_clear(editor))
+    entry_rightkey_menu.post(event.x_root,event.y_root)
 
 if __name__ == "__main__":
     root = tkinter.Tk()
@@ -329,12 +451,15 @@ if __name__ == "__main__":
     L1 = tkinter.Label(root, textvariable = file_name_str)
     L1.place(x=0, y=0, width=560,height=32)
 
-    EtL1 = tkinter.Label(root, text = "名称/命令")
+    entry_rightkey_menu = tkinter.Menu(root,tearoff=False)
+
+    EtL1 = tkinter.Label(root, text = "名称/指令")
     EtL1.place(x=0, y=432, width=60,height=32)
     t1 = tkinter.StringVar()
-    t1.set("名称与命令")
+    t1.set("名称与指令")
     Et1 = tkinter.Entry(root, textvariable=t1)
     Et1.place(x=60, y=432, width=500, height=32)
+    Et1.bind("<Button-3>", lambda x: entry_rightKey(x, Et1))
 
     EtL2 = tkinter.Label(root, text = "作者")
     EtL2.place(x=0, y=464, width=60,height=32)
@@ -342,6 +467,7 @@ if __name__ == "__main__":
     t2.set("作者")
     Et2 = tkinter.Entry(root, textvariable=t2)
     Et2.place(x=60, y=464, width=500, height=32)
+    Et2.bind("<Button-3>", lambda x: entry_rightKey(x, Et2))
 
     EtL3 = tkinter.Label(root, text = "版本")
     EtL3.place(x=0, y=496, width=60,height=32)
@@ -349,6 +475,7 @@ if __name__ == "__main__":
     t3.set("版本")
     Et3 = tkinter.Entry(root, textvariable=t3)
     Et3.place(x=60, y=496, width=500, height=32)
+    Et3.bind("<Button-3>", lambda x: entry_rightKey(x, Et3))
 
     EtL4 = tkinter.Label(root, text = "描述")
     EtL4.place(x=0, y=528, width=60,height=32)
@@ -356,6 +483,7 @@ if __name__ == "__main__":
     t4.set("描述")
     Et4 = tkinter.Entry(root, textvariable=t4)
     Et4.place(x=60, y=528, width=500, height=32)
+    Et4.bind("<Button-3>", lambda x: entry_rightKey(x, Et4))
 
     EtL5 = tkinter.Label(root, text = "子指令")
     EtL5.place(x=0, y=560, width=60,height=32)
@@ -363,6 +491,7 @@ if __name__ == "__main__":
     t5.set("子指令")
     Et5 = tkinter.Entry(root, textvariable=t5)
     Et5.place(x=60, y=560, width=500, height=32)
+    Et5.bind("<Button-3>", lambda x: entry_rightKey(x, Et5))
 
     EtL6 = tkinter.Label(root, text = "Default")
     EtL6.place(x=0, y=592, width=60,height=32)
@@ -370,6 +499,7 @@ if __name__ == "__main__":
     t6.set("Default")
     Et6 = tkinter.Entry(root, textvariable=t6)
     Et6.place(x=60, y=592, width=500, height=32)
+    Et6.bind("<Button-3>", lambda x: entry_rightKey(x, Et6))
 
     #Btn1 = tkinter.Button(root, text = "选择文件", command = select_file)
     #Btn1.place(x=500, y=0, width=60, height=32)
@@ -380,11 +510,14 @@ if __name__ == "__main__":
 
     menu_bar = tkinter.Menu(root)
     file_menu = tkinter.Menu(menu_bar, tearoff=0)
+    set_menu = tkinter.Menu(menu_bar, tearoff=0)
     info_menu = tkinter.Menu(menu_bar, tearoff=0)
     menu_bar.add_cascade(label="文件", menu=file_menu)
+    menu_bar.add_cascade(label="操作", menu=set_menu)
     menu_bar.add_cascade(label="关于", menu=info_menu)
     file_menu.add_command(label="导入文件", command=select_file)
     file_menu.add_command(label="开始转换", command=tran_save)
+    set_menu.add_command(label="清空所有设置栏", command=clear_conf)
     info_menu.add_command(label="关于", command=show_info)
     info_menu.add_command(label="查看项目", command=show_project_site)
     root.config(menu=menu_bar)
@@ -406,6 +539,8 @@ if __name__ == "__main__":
 
     tree = ttk.Treeview(root)
     tree.place(x=0, y=32, width=560, height=400)
+    tree_rightkey_menu = tkinter.Menu(root,tearoff=False)
+    tree.bind("<Button-3>", lambda x: tree_rightKey(x, tree))
 
     progress_obj = ttk.Progressbar(root, orient="horizontal", length=560, mode="determinate")
     progress_obj.place(x=0, y=32, width=560, height=25)
